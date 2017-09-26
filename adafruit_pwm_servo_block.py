573c79a1851e6a47b16359105074cb2db90f5656
Tyler Lugger <tlugger@n.io>
2017-09-26 09:02:06 -0600

    Adding block wrapper

--- adafruit_pwm_servo_block.py
+++ adafruit_pwm_servo_block.py
@@ -1,34 +1,21 @@
 from nio.block.base import Block
 from nio.properties import VersionProperty
-from __future__ import division
-import time
 
+import time
 import Adafruit_PCA9685
 
+class AdafruitPWMServo(Block)
 
-pwm = Adafruit_PCA9685.PCA9685()
+    def __init__(self):
+            super().__init__()
+            self.pwm = Adafruit_PCA9685.PCA9685()
+            self.servo_min = 200
+            self.servo_max = 550
+            self.pwm.set_pwm_freq(60)
 
-servo_min = 200
-servo_max = 550
-
-# # Helper function to make setting a servo pulse width simpler.
-# def set_servo_pulse(channel, pulse):
-#     pulse_length = 1000000    # 1,000,000 us per second
-#     pulse_length //= 60       # 60 Hz
-#     print('{0}us per period'.format(pulse_length))
-#     pulse_length //= 4096     # 12 bits of resolution
-#     print('{0}us per bit'.format(pulse_length))
-#     pulse *= 1000
-#     pulse //= pulse_length
-#     pwm.set_pwm(channel, 3, pulse)
-
-# Set frequency to 60hz, good for servos.
-pwm.set_pwm_freq(60)
-
-print('Moving servo on channel 0, press Ctrl-C to quit...')
-while True:
-    # Move servo on channel O between extremes.
-    pwm.set_pwm(0, 0, servo_min)
-    time.sleep(1)
-    pwm.set_pwm(0, 0, servo_max)
-    time.sleep(1)
+    def process_signals(self, signals):
+        for signal in signals:
+            self.pwm.set_pwm(0, 0, servo_min)
+            time.sleep(1)
+            self.pwm.set_pwm(0, 0, servo_max)
+            time.sleep(1)
