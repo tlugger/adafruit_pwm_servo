0758df75b64ffce58c2002f0912f29ebb2caadc8
Tyler Lugger <tlugger@n.io>
2017-09-26 09:27:53 -0600

    Add configuration optons to block

--- adafruit_pwm_servo_block.py
+++ adafruit_pwm_servo_block.py
@@ -1,21 +1,22 @@
 from nio.block.base import Block
-from nio.properties import VersionProperty
+from nio.properties import VersionProperty, IntProperty
 
-import time
 import Adafruit_PCA9685
 
 class AdafruitPWMServo(Block):
 
+    pulse_length = IntProperty(title="Pulse Length", default=0)
+    frequency = IntPropety(title="Frequency", default=60)
+    servo_channel = IntProperty(title="Servo Channel", defalut=0)
+
     def __init__(self):
             super().__init__()
             self.pwm = Adafruit_PCA9685.PCA9685()
-            self.servo_min = 200
-            self.servo_max = 550
-            self.pwm.set_pwm_freq(60)
+            self.pulse_length = None
+
+    def start(self):
+            self.pwm.set_pwm_freq(self.frequency())
 
     def process_signals(self, signals):
         for signal in signals:
-            self.pwm.set_pwm(0, 0, self.servo_min)
-            time.sleep(1)
-            self.pwm.set_pwm(0, 0, self.servo_max)
-            time.sleep(1)
+            self.pwm.set_pwm(self.servo_channel(), 0, self.pulse_length())
